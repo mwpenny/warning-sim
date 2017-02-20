59dffac45bea8bcbcbdecfb3b1feefccf4388c03
Matt Penny <mwpenny@users.noreply.github.com>
2017-02-19 21:46:23 -0500

    Implement control warnings, touch up tiered layout

--- background.js
+++ background.js
@@ -1,57 +1,84 @@
 /* Background script for sending warning message contents to warning pages */
 
-// TODO: control pages
+var tieredStrings = [
+	{
+		title: "Suspicious page",
+		instructions: "This page may be dangerous. Proceed with caution.",
+		class: "warning-mild"
+	},
+	{
+		title: "Untrusted page",
+		instructions: "It is probably not safe to browse to this page.",
+		class: "warning-medium"
+	},
+	{
+		title: "Potential threat detected!",
+		instructions: "<b>It is highly reccomended that you do not continue.</b>",
+		class: "warning-severe"
+	}
+];
 
-/* Content for tiered warning messages. 0 = mild, 1 = medium, 2 = severe */
-var messages = [
+var warnings = [
 	// SSL
-	[
-		"The authenticity of $, and thus its security, was unable to be verified.",
-		"The identity of $ could not be confirmed and therefore its security cannot be guaranteed. Information sent may be visible to others.",
-		"A secure connection could not be established to $. While browsing this website, it is possible for an attacker to monitor your activity and steal your personal information (passwords, financial information, conversations, etc.).",
-	],
+	{
+		controlTitle: "Your connection is not secure",
+		tieredMessages: [
+			"The authenticity of <b>$</b>, and thus its security, was unable to be verified.",
+			"The identity of <b>$</b> could not be confirmed and therefore its security cannot be guaranteed. Information sent may be visible to others.",
+			"A secure connection could not be established to <b>$</b>. While browsing this website, it is possible for an attacker to monitor your activity and steal your personal information (passwords, financial information, conversations, etc.).",
+			"The owner of $ has configured their website improperly. To protect your information from being stolen, Firefox has not connected to this website."
+		],
+		tieredMoreInfo: "Secure websites identify themselves using certificates. When a certificate is unable to be verified, it can mean that security settings have been misconfigured, security of the website has been compromised, or that an attacker is impersonating the website. In any case, any personal information entered into the page is at risk of being seen by others (e.g., passwords and credit card numbers).",
+		controlMoreInfo: "$ uses an invalid security certificate.<br/><br/>The certificate is not trusted because the issuer certificate is unknown. The server might not be sending the appropriate intermediate certificates. An additional root certificate may need to be imported.",
+		controlLearnLink: "https://support.mozilla.org/kb/what-does-your-connection-is-not-secure-mean",
+		controlBackButtonText: "Go Back",
+		controlMoreInfoButtonText: "Advanced"
+	},
 	// Malware
-	[
-		"$ may contain software which can harm your computer.",
-		"$ has been reported by users in the past as being dangerous. It could contain malware.",
-		"$ has been reported by users and contains software which could infect and harm your computer. It may also monitor keystrokes and steal personal information such as passwords and banking details.",
-	],
+	{
+		controlTitle: "Reported Attack Page!",
+		tieredMessages: [
+			"<b>$</b> may contain software which can harm your computer.",
+			"<b>$</b> has been reported by users in the past as being dangerous. It could contain malware.",
+			"<b>$</b> has been reported by users and contains software which could infect and harm your computer. It may also monitor keystrokes and steal personal information such as passwords and banking details.",
+			"This web page at $ has been reported as an attack page and has been blocked based on your security preferences."
+		],
+		tieredMoreInfo: "Pages containing harmful software (malware) attempt to infect and take over your web browser and computer by installing malicious applications. Such software can allow attackers to steal personal information, delete files, and infect others.",
+		controlMoreInfo: "<p>Attack pages try to install programs that steal private information, use your computer to attack others, or damage your system.</p><p>Some attack pages intentionally distribute harmful software, but many are compromised without the knowledge or permission of their owners.</p>",
+		controlLearnLink: "https://support.mozilla.org/t5/Protect-your-privacy/How-does-built-in-Phishing-and-Malware-Protection-work/ta-p/9395",
+		controlBackButtonText: "Get me out of here!",
+		controlMoreInfoButtonText: "Why was this page blocked?"
+	},
 	// Phishing
-	[
-		"The identity of the page at $, and thus the source of its contents, could not be verified.",
-		"Past users have reported that $ is fake. It may steal information entered into it.",
-		"$ has been reported by users and it is likely impersonating another page. Any personal information entered (passwords, financial information, conversations, etc.) could be stolen.",
-	],
+	{
+		controlTitle: "Deceptive Site!",
+		tieredMessages: [
+			"The identity of the page at <b>$</b>, and thus the source of its contents, could not be verified.",
+			"Past users have reported that <b>$</b> is fake. It may steal information entered into it.",
+			"<b>$</b> has been reported by users and it is likely impersonating another page. Any personal information entered (passwords, financial information, conversations, etc.) could be stolen.",
+			"This web page at $ has been reported as a deceptive site and has been blocked based on your security preferences."
+		],
+		tieredMoreInfo: "Imitation pages impersonate sources you may trust and allow criminals to steal sensitive information such as passwords and credit card numbers. These pages may look and behave as expected, however information entered into them is actually sent to a criminal - not the organization the page claims to represent.",
+		controlMoreInfo: "<p>Deceptive sites are designed to trick you into doing something dangerous, like installing software, or revealing your personal information, like passwords, phone numbers or credit cards.</p><p>Entering any information on this web page may result in identity theft or other fraud.</p>",
+		controlLearnLink: "https://support.mozilla.org/t5/Protect-your-privacy/How-does-built-in-Phishing-and-Malware-Protection-work/ta-p/9395",
+		controlBackButtonText: "Get me out of here!",
+		controlMoreInfoButtonText: "Why was this page blocked?"
+	},
 	// Unwanted software
-	[
-		"$ may contain potentially misleading and deceptive software.",
-		"Users have reported that $ contains potentially unwanted software which it tries to install.",
-		"$ contains potentially dangerous and unwanted software which can negatively impact your computer. Users have reported that the page also employs trickery to encourage downloads of this software (e.g., fake download buttons).",
-	],
-];
-var titles = [
-	"Suspicious page",
-	"Untrusted page",
-	"Potential threat detected!"
-];
-var instructions = [
-	"This page may be dangerous. Proceed with caution.",
-	"It is probably not safe to browse to this page.",
-	"<b>It is highly reccomended that you do not continue.</b>"
-];
-var classes = [
-	"warning-mild",
-	"warning-medium",
-	"warning-severe"
-];
-
-/* Text for the "more info" section of the page.
-   0 = SSL, 1 = malware, 2 = phishing, 3 = unwanted software */
-var moreInfo = [
-	"Secure websites identify themselves using certificates. When a certificate is unable to be verified, it can mean that security settings have been misconfigured, security of the website has been compromised, or that an attacker is impersonating the website. In any case, any personal information entered into the page is at risk of being seen by others (e.g., passwords and credit card numbers).",
-	"Pages containing harmful software (malware) attempt to infect and take over your web browser and computer by installing malicious applications. Such software can allow attackers to steal personal information, delete files, and infect others.",
-	"Imitation pages impersonate sources you may trust and allow criminals to steal sensitive information such as passwords and credit card numbers. These pages may look and behave as expected, however information entered into them is actually sent to a criminal - not the organization the page claims to represent.",
-	"Unwanted software is classified as software that may behave in ways that you do not approve of or are not aware of. Examples include adware and spyware. Such software may also attempt to install additional unwanted software without your consent. Pages that distribute unwanted software often encourage downloads through deception (e.g., fake links, fake download buttons, and fake exit buttons)."
+	{
+		controlTitle: "Reported Unwanted Software Page!",
+		tieredMessages: [
+			"<b>$</b> may contain potentially misleading and deceptive software.",
+			"Users have reported that <b>$</b> contains potentially unwanted software which it tries to install.",
+			"<b>$</b> contains potentially dangerous and unwanted software which can negatively impact your computer. Users have reported that the page also employs trickery to encourage downloads of this software (e.g., fake download buttons).",
+			"This web page at $ has been reported to contain unwanted software and has been blocked based on your security preferences."
+		],
+		tieredMoreInfo: "Unwanted software is classified as software that may behave in ways that you do not approve of or are not aware of. Examples include adware and spyware. Such software may also attempt to install additional unwanted software without your consent. Pages that distribute unwanted software often encourage downloads through deception (e.g., fake links, fake download buttons, and fake exit buttons).",
+		controlMoreInfo: "<p>Unwanted software pages try to install software that can be deceptive and affect your system in unexpected ways.</p>",
+		controlLearnLink: "https://support.mozilla.org/t5/Protect-your-privacy/How-does-built-in-Phishing-and-Malware-Protection-work/ta-p/9395",
+		controlBackButtonText: "Get me out of here!",
+		controlMoreInfoButtonText: "Why was this page blocked?"
+	}
 ];
 
 var domains = {};
@@ -69,13 +96,29 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 		var response = {triggered: domains.hasOwnProperty(message.domain)};
 		if (response.triggered) {
 			var domainInfo = domains[message.domain];
-			var msg = messages[domainInfo.type][domainInfo.severity];
-			response.message = msg.replace("$", "<b>" + message.domain + "</b>");
-			response.moreInfo = moreInfo[domainInfo.type];
-			response.title = titles[domainInfo.severity];
-			response.instructions = instructions[domainInfo.severity];
-			response.class = classes[domainInfo.severity];
+			var warning = warnings[domainInfo.type];
+			var msg = warning.tieredMessages[domainInfo.severity];
+			response.message = msg.replace("$", message.domain);
+
+			if (domainInfo.severity == 3) {
+				// 3 = control warning message
+				response.moreInfo = warning.controlMoreInfo;
+				response.title = warning.controlTitle;
+				response.learnLink = warning.controlLearnLink;
+				response.backButtonText = warning.controlBackButtonText;
+				response.moreInfoButtonText = warning.controlMoreInfoButtonText;
+			} else {
+				var ts = tieredStrings[domainInfo.severity];
+				response.moreInfo = warning.tieredMoreInfo;
+				response.title = ts.title;
+				response.instructions = ts.instructions;
+				response.class = ts.class;
+				response.backButtonText = "Go Back";
+				response.moreInfoButtonText = "More information";
+			}
+			response.moreInfo = response.moreInfo.replace("$", message.domain);
 			response.severity = domainInfo.severity;
+			response.type = domainInfo.type;
 		}
 		sendResponse(response);
 	}
--- warning/loader.js
+++ warning/loader.js
@@ -1,59 +1,121 @@
-/* Loader for injecting warning messages in pages */
+/* Loader for injecting warning messages into pages */
 
 var resBase = chrome.runtime.getURL("warning/");
 
-function styleWarning(warningInfo) {
-	// Style page depending on warning type
+function insertWarningText(warning) {
+	// Places warning-specific text into the page
 	var wTitle = document.getElementsByClassName("warning-title")[0];
-	var wBody = document.getElementsByClassName("warning-body")[0];
+	wTitle.insertBefore(document.createTextNode(warning.title), wTitle.firstChild);
+	document.getElementsByClassName("warning-body")[0].innerHTML = warning.message;
+	document.getElementsByClassName("warning-more-info")[0].innerHTML += warning.moreInfo;
+	document.title = warning.title;
+	document.head.innerHTML += "<link rel='icon' type='image/png' href='chrome://global/skin/icons/warning-16.png'/>";
 
-	wTitle.insertBefore(document.createTextNode(warningInfo.title), wTitle.firstChild);
-	wBody.innerHTML = warningInfo.message + "<br/><br/>" + warningInfo.instructions;
-	document.getElementsByClassName("warning-more-info")[0].innerHTML += warningInfo.moreInfo;
-	document.title = warningInfo.title;
-	document.body.className = warningInfo.class;
+	var back = document.getElementsByClassName("warning-back-button")[0];
+	var more = document.getElementsByClassName("warning-more-button")[0];
+
+	back.innerHTML = warning.backButtonText;
+	more.innerHTML = warning.moreInfoButtonText;
+
+	// "More information" button
+	more.onclick = function() {
+		var infoBox = document.getElementsByClassName("warning-more-info")[0];
+		if (infoBox.style.display != "block")
+			infoBox.style.display = "block";
+		else
+			infoBox.style.display = "none";
+	};
+
+	back.addEventListener("click", function() {
+		window.history.back();
+	});
+
+	// TODO: "ignore this warning" listener
+};
+
+function styleTieredWarning(warning) {
+	// Style page depending on warning type
+	document.getElementsByClassName("warning-body")[0].innerHTML += "<br/><br/>" + warning.instructions;
+	document.body.className = warning.class;
 
 	// Higher threat level -> more warning icons
-	var i;
 	var wIcons = document.getElementsByClassName("warning-icons")[0].getElementsByTagName("object");
-	for (i = 0; i < 3; ++i) {
+	for (var i = 0; i < 3; ++i) {
 		var svg;
-		if (i <= warningInfo.severity)
+		if (i <= warning.severity)
 			svg = "warning.svg";
 		else
 			svg = "warning-outline.svg";
 		wIcons[i].setAttribute("data", resBase + svg);
 	}
+};
 
-	// "More information" button
-	var moreLink = document.getElementsByClassName("warning-more-link")[0];
-	moreLink.addEventListener("click", function() {
-	    document.getElementsByClassName("warning-more-info")[0].style.display = "block";
-	    moreLink.style.visibility = "hidden";
+function styleControlWarning(warning) {
+	var learnLink = document.getElementsByClassName("warning-learn-link")[0];
+	if (warning.type > 0) {
+		// Not an SSL warning - "more info" button opens link to info in new tab
+		var more = document.getElementsByClassName("warning-more-button")[0];
+		more.onclick = function() {
+			window.open(warning.learnLink);
+		};
+		learnLink.parentNode.remove();
+		document.getElementsByClassName("warning-body")[0].innerHTML += warning.moreInfo;
+	} else {
+		learnLink.href = warning.learnLink;
+	}
+}
+
+function xhr(url, cb) {
+	// Performs an AJAX call
+	var req = new XMLHttpRequest();
+	req.addEventListener("load", cb);
+	req.open("GET", url);
+	req.send();
+}
+
+function loadTieredWarning(warning) {
+	// Loads tiered warning message
+	xhr(resBase + "tiered.html", function(e) {
+		document.head.innerHTML = "<link rel='stylesheet' type='text/css' href='" + resBase + "tiered.css'>";
+		document.body.innerHTML = e.target.responseText;
+		insertWarningText(warning);
+		styleTieredWarning(warning);
 	});
 };
 
-function loadWarning(warningInfo) {
-	// Construct warning message in the DOM
-	var req = new XMLHttpRequest();
-	req.addEventListener("load", function(e) {
-		document.head.innerHTML = "<link rel='stylesheet' type='text/css' href='" + resBase + "warnings.css'>";
+function loadControlWarning(warning) {
+	// Loads control warning message
+	xhr(resBase + "control.html", function(e) {
+		var css;
+		// SSL warning
+		if (warning.type == 0) {
+			css = "chrome://browser/skin/aboutNetError.css";
+			document.body.className = "certerror";
+		}
+		else {
+			css = "chrome://browser/skin/blockedSite.css"
+		}
+		document.head.innerHTML = "<link rel='stylesheet' type='text/css' href='" + css + "'>";
 		document.body.innerHTML = e.target.responseText;
-		styleWarning(warningInfo);
+		insertWarningText(warning);
+		styleControlWarning(warning);
 	});
-	req.open("GET", resBase + "warning.html");
-	req.send();
 };
 
 // Does this domain trigger a warning?
-chrome.runtime.sendMessage({domain: document.domain}, function(response) {
-	if (response && response.triggered) {
+chrome.runtime.sendMessage({domain: document.domain}, function(warning) {
+	if (warning && warning.triggered) {
 		// We need a clean DOM
 		window.stop();
 		document.removeChild(document.documentElement);
 		document.appendChild(document.createElement("html"));
 		document.documentElement.appendChild(document.createElement("head"));
 		document.documentElement.appendChild(document.createElement("body"));
-		loadWarning(response);
+
+		// 0 = mild, 1 = medium, 2 = severe, 3 = control
+		if (warning.severity == 3)
+			loadControlWarning(warning);
+		else
+			loadTieredWarning(warning);
 	}
 });
